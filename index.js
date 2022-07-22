f7dc129bd85680aff5a9e0d202e20f78b35de541
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-07-22 22:35:44 +0800

    create fund function

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
+import { ethers } from "./ethers-5.2.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
-    document.getElementById("connectButton").innerHTML = "Connected!";
+    connectButton.innerHTML = "Connected!";
   } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install Metamask!";
+    connectButton.innerHTML = "Please install Metamask!";
   }
+}
+
+async function fund() {
+  const ethAmount = "0.1";
+  console.log(`Funding with ${ethAmount}...`);
+
+  if (typeof window.ethereum !== "undefined") {
+    // provider / connection to the blockchain
+    // signer / wallet / someone with some gas
+    // contract that we are interacting with
+    // ^ ABI & address
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("Done!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`);
+  // listen for this transaction to finish
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(
+        `Completed with ${transactionReceipt.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
 }
